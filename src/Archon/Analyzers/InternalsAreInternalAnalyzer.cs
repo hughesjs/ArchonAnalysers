7ac20a59f1bdcdfedad8b4fa17ed592db4e0727f
James Hughes <james@pyrosoftsolutions.co.uk>
2025-12-21 07:09:09 +0000

    chore:  Removed unused using

--- src/Archon/Analyzers/InternalsAreInternalAnalyzer.cs
+++ src/Archon/Analyzers/InternalsAreInternalAnalyzer.cs
@@ -3,8 +3,6 @@ using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
-using Microsoft.CodeAnalysis.Text;
-
 namespace Archon.Analyzers;
 
 [DiagnosticAnalyzer(LanguageNames.CSharp)]
